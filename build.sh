3366f1d0910d75d6eaaee8c50713d89ea941f0d5
Dan Klishch <danilklishch@gmail.com>
2020-10-26 20:44:02 +0500

    Fix pipeline

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 mkdir build
+mkdir build/sections
 working_dir=`pwd`
 error=false
-while read a b
+
+while read a b 
 do
 	cd $b
-	if pdflatex -output-directory="$working_dir/build" -jobname=$a main.tex &&
-	   pdflatex -output-directory="$working_dir/build" -jobname=$a main.tex ; then
+	if pdflatex -output-directory="$working_dir/build" -jobname=$a main.tex && pdflatex -output-directory="$working_dir/build" -jobname=$a main.tex ; then
 		echo -e "\e[32m[OK]\e[0m $b"
 	else
 		echo -e "\e[31m[FAILED]\e[0m $b"
